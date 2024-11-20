f3f2d6bada5df38a974b10c4187ea90f48420b8f
vivianrcc <vivianrosanacc@gmail.com>
2024-11-20 19:04:59 -0300

    add new class exercise01

---
+++ src/Exercise01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/*
+Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
+mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
+(Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)
+ */
+
+import java.util.Scanner;
+
+public class Exercise01 {
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //ENTRADA DE DADOS
+
+        int idade;
+
+        System.out.println("Digite sua idade: Exemplo X anos, X meses e X dias \n");
+        idade = scanner.nextInt();
+
+
+    }
+
+}
